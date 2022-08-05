432f4a7a8654a4acb4d136f1dc0d556416ad1e83
+ <+>
2022-08-06 05:46:11 +1000

    js displaying weather data in code, correctly working on dom, next is to make sure search bar working

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ let weather = {
             + city 
             + "&units=metric&appid=" 
             + this.apiKey
-            )
+         )
             .then((response) => response.json())
-            .then((data) => console.log(data)); 
+            .then((data) => this.displayWeather(data)); 
         },
         displayWeather: function (data) {
-        }
-};
+            const { name } = data; 
+            const { icon, description } = data.weather[0];
+            const { temp, humidity } = data.main; 
+            const { speed } = data.wind;
+            console.log (name, icon, description, temp, humidity, speed);
+            //Get the data to display on page//
+            document.querySelector(".city").innerText = "Weather in " + name; 
+            document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png"; 
+            document.querySelector(".description").innerText = description; 
+            document.querySelector(".temp").innerText = temp + "°C";
+            document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
+            document.querySelector(".wind").innerText = "Wind Speed:" + speed + "km/h";
+         },
+        };
